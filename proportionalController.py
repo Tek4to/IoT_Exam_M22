a5e8fd0d39e83bde4c59352604271cddb76ac7c4
Daniil <55653808+Tek4to@users.noreply.github.com>
2023-06-19 15:35:19 +0600

    Add files via upload
    Update code for txt files instead of excel file. Add comments.

--- proportionalController.py
+++ proportionalController.py
@@ -1,10 +1,11 @@
 from datetime import datetime, timedelta
 import time
-import openpyxl
-from openpyxl import load_workbook
 
-wb = load_workbook('F5.xlsx', data_only=True)
-ws = wb.active
+# import openpyxl
+# from openpyxl import load_workbook
+
+# wb = load_workbook('F5.xlsx', data_only=True)
+# ws = wb.active
 
 DT_INTERVAL = None
 DT_TIME = None
@@ -15,6 +16,7 @@ my_file = open("F6.txt", "w+")
 my_file.close()
 
 
+#  Parse F3 file to global variables and make borders lesser
 def parse_f3_file(f3_file):
     with open(f3_file) as file:
         line = file.readline()
@@ -29,33 +31,40 @@ def parse_f3_file(f3_file):
     TMIN += new_borders
 
 
+# Get temperature from last row from F5 file every method call
 def parse_f5_file(f5_file):
     with open(f5_file) as file:
         line = file.readlines()
     return int(line[-1].split()[-1])
 
 
-def check_temperature(i):
-    # current_t = parse_f5_file('F5.txt')
-    current_t = ws.cell(row=i, column=2).value
+# Get current temperature and check if it is between new borders. Write in file '1' and time when it sended
+def check_temperature():
+    current_t = parse_f5_file('F5.txt')
+    # current_t = ws.cell(row=i, column=2).value
     if current_t / TMAX > 1:
         with open('F6.txt', 'a+') as f:
-            f.write(f"1 {current_t}\n")
+            f.write(f"1 {datetime.now()}")
+            return 1
+            # f.write(f"1 {current_t}\n")
         # print(f'1 {current_t}')
     if current_t / TMIN < 1:
         with open('F6.txt', 'a+') as f:
-            f.write(f"1 {current_t}\n")
+            f.write(f"1 {datetime.now()}")
+            return 1
+            # f.write(f"1 {current_t}\n")
         # print(f'1 {current_t}')
 
 
+# Timer which check F5 file every tick
 def controller():
     timing = time.time()
     end_time = datetime.now() + timedelta(seconds=DT_TIME)
-    i = 2
+    # i = 2
     while datetime.now() <= end_time:
         if float(time.time() - timing) > float(DT_INTERVAL):
-            check_temperature(i)
-            i += 1
+            check_temperature()
+            # i += 1
             timing = time.time()
 
 
